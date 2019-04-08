6402c62121e1c0c7572f5c8a336ef934d179e00d
akepa <akepa@hotelbeds.com>
2019-04-08 23:18:28 +0200

    Add festival type

--- dataextractor.py
+++ dataextractor.py
@@ -15,7 +15,8 @@ rp = urllib.robotparser.RobotFileParser()
 rp.set_url(base_url + "robots.txt")
 rp.read()
 
-# TODO - PREMIO Y FESTIVALES
+
+# TODO - PAIS FESTIVAL
 # TODO - GENERO
 def main():
     print("Start.")
@@ -24,32 +25,34 @@ def main():
 
     with open('film-awards-dataset.csv', 'w', newline='', encoding='utf-8') as film_file:
 
-        fieldnames = ['award', 'award_year', 'title', 'year', 'director', 'country', 'score', 'nvotes']
+        fieldnames = ['award_name', 'award_type', 'award_year', 'film_title', 'film_year', 'film_director',
+                      'film_country', 'score', 'nvotes']
         writer = csv.DictWriter(film_file, fieldnames, delimiter=';')
         writer.writeheader()
 
-        awards = get_awards()
-        print("Extracted " + str(len(awards)) + " items")
-        for award_id, award_name in awards.items():
-            main_category_id = get_main_category_id(award_id)
-            if main_category_id is not None:
-                winner_dict = get_main_category_winners(award_id, main_category_id)
-                # print(winner_dict)
-                for year, movie_id in winner_dict.items():
+        award_list = get_awards()
+        for award_type, awards in award_list.items():
+            for award_id, award_name in awards.items():
+                main_category_id = get_main_category_id(award_id)
+                if main_category_id is not None:
+                    winner_dict = get_main_category_winners(award_id, main_category_id)
+                    # print(winner_dict)
+                    for year, movie_id in winner_dict.items():
 
-                    movie = None
-                    if movie_id in cache:
-                        movie = cache[movie_id]
-                    else:
-                        movie = get_movie_data(movie_id)
-                        cache[movie_id] = movie
-                    if "TV Series" in movie['title']:
-                        # Stop processing if it is a TV serie
+                        movie = None
+                        if movie_id in cache:
+                            movie = cache[movie_id]
+                        else:
+                            movie = get_movie_data(movie_id)
+                            cache[movie_id] = movie
+                        if "TV Series" in movie['film_title']:
+                            # Stop processing if it is a TV serie
+                            break
+                        movie['award_name'] = award_name
+                        movie['award_type'] = award_type
+                        movie['award_year'] = year
+                        writer.writerow(movie)
                         break
-                    movie['award'] = award_name
-                    movie['award_year'] = year
-                    writer.writerow(movie)
-                    break
 
 
 def download(url):
@@ -74,17 +77,26 @@ def get_main_category_id(festival_id):
 def get_awards():
     awards = {}
 
-    next_url = base_url + "es/all_awards.php?order=all"
+    next_url = base_url + "es/all_awards.php?order=bytype"
     soup = BeautifulSoup(download(next_url), 'html.parser')
 
     all_awards_list = soup.find('div', {"class": "all-awards-list"})
-    for a in all_awards_list.findAll('a'):
-        if a.has_attr('title'):
-            link = a.get('href')
-            award_id = link.split("award_id=")[1]
-            award_name = a.get('title')
-            awards[award_id] = award_name
+    award_content = all_awards_list.findAll('div', {'class': 'award-by-type-content'})
+
+    awards['Festival'] = get_awards_from_list(award_content[0])
+    awards['Award'] = get_awards_from_list(award_content[1])
+    awards['Association Award'] = get_awards_from_list(award_content[2])
     # print (awards)
+    return awards
+
+
+def get_awards_from_list(award_list):
+    awards = {}
+    for a in award_list.findAll('a'):
+        link = a.get('href')
+        award_id = link.split("award_id=")[1]
+        award_name = a.text
+        awards[award_id] = award_name
     return awards
 
 
@@ -149,11 +161,11 @@ def get_movie_data(movie_id):
     movie_info = soup.find('dl', {"class": "movie-info"})
     items = movie_info.findAll('dd')
 
-    movie['title'] = items[0].text.strip()
-    movie['year'] = items[1].text.strip()
-    movie['country'] = items[3].text.strip()
+    movie['film_title'] = items[0].text.strip()
+    movie['film_year'] = items[1].text.strip()
+    movie['film_country'] = items[3].text.strip()
 
-    movie['director'] = get_directors(items[4])
+    movie['film_director'] = get_directors(items[4])
 
     score_field = soup.find('div', {'id': 'movie-rat-avg'})
     votes_field = soup.find('span', {'itemprop': 'ratingCount'})
